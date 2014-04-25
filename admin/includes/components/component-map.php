b245981d2adbfd711aa10dafd541630c6cac8a24
nphaskins <nphaskins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-25 12:59:23 +0000

    1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/aesop-story-engine/tags/1.0.4@902617 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/includes/components/component-map.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ 	* Filters custom meta box class to add cusotm meta to map component
+ 	*
+ 	* @since    1.0.0
+*/
+class AesopMapComponentAdmin {
+
+	public function __construct(){
+		add_filter( 'cmb_meta_boxes', array($this,'aesop_map_meta') );
+	}
+
+	function aesop_map_meta( array $meta_boxes ) {
+
+		$opts = array(
+			array(
+				'id'			=> 'aesop_map_start',
+				'name'			=> __('Starting Coordinates', 'aesop-core'),
+				'type'			=> 'text',
+			),
+			array(
+				'id'			=> 'aesop_map_component_zoom',
+				'name'			=> __('Default Zoom Level', 'aesop-core'),
+				'type'			=> 'text',
+				'desc'			=> __('The larger the number, the more zoomed in the default will be. Limit is 20. Default is 12.','aesop-core')
+			),
+			array(
+				'id' 			=> 'aesop_map_component_locations',
+				'name' 			=> __('Map Markers', 'aesop-core'),
+				'type' 			=> 'group',
+				'repeatable'     => true,
+				'repeatable_max' => 20,
+				'desc'			=> __('Assign latitude and longitude for each marker.', 'aesop-core'),
+				'fields' 		=> array(
+					array(
+						'id' 	=> 'lat',
+						'name' 	=> __('Latitude', 'aesop-core'),
+						'type' 	=> 'text',
+						'cols'	=> 6
+					),
+					array(
+						'id' 	=> 'long',
+						'name' 	=> __('Longitude', 'aesop-core'),
+						'type' 	=> 'text',
+						'cols'	=> 6
+					),
+					array(
+						'id' 	=> 'content',
+						'name' 	=> __('Marker Text', 'aesop-core'),
+						'type' 	=> 'text',
+						'cols'	=> 12
+					)
+				)
+			),
+		);
+
+		$meta_boxes[] = array(
+			'title' => __('Map Component Locations', 'aesop-core'),
+			'pages' => 'post',
+			'context' => 'side',
+			'fields' => $opts
+		);
+
+		return $meta_boxes;
+
+	}
+
+}
+new AesopMapComponentAdmin;
---
+++ public/includes/components/component-quote.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ 	* Audio component utilizes core wordpress audio
+ 	*
+ 	* @since    1.0.0
+*/
+if (!function_exists('aesop_quote_shortcode')){
+
+	function aesop_quote_shortcode($atts, $content = null) {
+
+		$defaults = array(
+			'width'		=> '100%',
+			'background' => '#222222',
+			'img'		=> '',
+			'text' 		=> '#FFFFFF',
+			'height'	=> 'auto',
+			'align'		=> 'left',
+			'size'		=> '4',
+			'parallax'  => '',
+			'speed' 	=> 8,
+			'direction' => '',
+			'offset'	=> 300,
+			'quote'		=> '',
+
+		);
+		$atts = apply_filters('aesop_quote_defaults',shortcode_atts($defaults, $atts));
+
+		// use multiple times
+		$hash = rand();
+
+		// set component to content width
+		$contentwidth = 'content' == $atts['width'] ? 'aesop-content' : false;
+
+		// set size
+		$size = $atts['size'] ? sprintf('%srem', $atts['size']) : false;
+
+		//bg img
+		$bgimg = $atts['img'] ? sprintf('background-image:url(%s);background-size:cover;background-position:center center',$atts['img']) : false;
+
+		// set styles
+		$style = $atts['background'] || $atts['text'] || $atts['height'] || $atts['width'] ? sprintf('style="background-color:%s;%s;color:%s;height:%s;width:%s;"',$atts['background'], $bgimg, $atts['text'], $atts['height'], $atts['width']) : false;
+
+		$isparallax = 'on' == $atts['parallax'] ? 'quote-is-parallax' : false;
+		$lrclass	= 'left' == $atts['direction'] || 'right' == $atts['direction'] ? 'quote-left-right' : false;
+
+		// clean offset
+		$offset = preg_replace('/[^0-9]/','',$atts['offset']);
+
+		// custom classes
+		$classes = function_exists('aesop_component_classes') ? aesop_component_classes( 'quote', '' ) : null;
+
+		ob_start();
+
+		do_action('aesop_quote_before'); //action
+		?>
+			<div id="aesop-quote-component-<?php echo $hash;?>" class="aesop-component aesop-quote-component <?php echo $classes.' '.$contentwidth.' '.$isparallax.' '.$lrclass.' ';?>" <?php echo $style;?>>
+
+				<!-- Aesop Core | Quote -->
+				<script>
+					jQuery(document).ready(function(){
+
+						var obj = jQuery('#aesop-quote-component-<?php echo $hash;?> blockquote');
+
+						// if parallax is on and we're not on mobile
+						<?php if ( 'on' == $atts['parallax'] && !wp_is_mobile() ) { ?>
+
+					       	function scrollParallax(){
+					       	    var floater = (jQuery(window).scrollTop() / <?php echo sanitize_text_field($atts['speed']);?>) - <?php echo $offset;?>;
+
+					            jQuery(obj).css({'transform':'translate3d(0px,-' + floater + 'px, 0px)'});
+
+					       	    <?php if ('left' == $atts['direction'] || 'right' == $atts['direction']){
+
+									if ('left' == $atts['direction']){ ?>
+					            		jQuery(obj).css({'transform':'translate3d(' + floater + 'px, 0px, 0px)'});
+					            	<?php } else { ?>
+										jQuery(obj).css({'transform':'translate3d(-' + floater + 'px, 0px, 0px)'});
+					            	<?php }
+
+					       	    } else {
+
+					       	    	if ('up' == $atts['direction']){ ?>
+					            		jQuery(obj).css({'transform':'translate3d(0px,' + floater + 'px, 0px)'});
+									<?php } else { ?>
+										jQuery(obj).css({'transform':'translate3d(0px,-' + floater + 'px, 0px)'});
+									<?php }
+					            } ?>
+					       	}
+					       	jQuery(obj).waypoint({
+								offset: 'bottom-in-view',
+								handler: function(direction){
+						   			jQuery(this).toggleClass('aesop-quote-faded');
+
+						   			// fire parallax
+						   			scrollParallax();
+									jQuery(window).scroll(function() {scrollParallax();});
+							   	}
+							});
+
+						<?php } else { ?>
+
+							jQuery(obj).waypoint({
+								offset: 'bottom-in-view',
+								handler: function(direction){
+							   		jQuery(this).toggleClass('aesop-quote-faded');
+
+							   	}
+							});
+						<?php } ?>
+
+					});
+				</script>
+
+				<?php do_action('aesop_quote_inside_top'); //action ?>
+
+				<blockquote class="aesop-component-align-<?php echo $atts['align'];?>" style="font-size:<?php echo $size;?>;">
+					<?php echo $atts['quote'];?>
+				</blockquote>
+
+				<?php do_action('aesop_quote_inside_bottom'); //action ?>
+
+			</div>
+		<?php
+		do_action('aesop_quote_after'); //action
+
+		return ob_get_clean();
+	}
+}
