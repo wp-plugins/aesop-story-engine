15dcac4764f2a2e6c814a2079088f8f683466eb3
nphaskins <nphaskins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-13 13:09:49 +0000

    1.0.7
    git-svn-id: https://plugins.svn.wordpress.org/aesop-story-engine/tags/1.0.7@931784 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public/includes/components/component-audio.php
@@ -0,0 +1,76 @@
+<?php
+
+/**
+ 	* Audio component utilizes core wordpress audio
+ 	*
+ 	* @since    1.0.0
+*/
+if (!function_exists('aesop_audio_shortcode')){
+
+	function aesop_audio_shortcode($atts, $content = null) {
+
+    	$defaults 	= array(
+    		'title'		=> '',
+    		'src' 		=>	'',
+    		'viewstart' => 'off',
+    		'viewend'	=>  'off',
+    		'loop'		=> 'off',
+    		'hidden'	=> ''
+    	);
+	    $atts 		= apply_filters('aesop_video_defaults',shortcode_atts($defaults, $atts));
+	    $hash 		= rand();
+
+		// custom classes
+		$classes = function_exists('aesop_component_classes') ? aesop_component_classes( 'audio', '' ) : null;
+
+		// hidden
+		$hidden = 'on' == $atts['hidden'] ? 'style="height:0;z-index:-1;position:absolute;opacity:0;"' : null;
+
+		// optional title
+		$title = $atts['title'] ? apply_filters('aesop_audio_component_title', sprintf('<h5>%s</h5>', $atts['title'])) : null;
+
+	    ob_start();
+
+	    do_action('aesop_audio_before'); //action
+
+	   		?><aside id="aesop-audio-<?php echo $hash;?>" class="aesop-component aesop-audio-component <?php echo $classes;?>" <?php echo $hidden;?>>
+
+	   			<?php if ('on' == $atts['viewstart']) { ?>
+			    	<script>
+			    	jQuery(document).ready(function(){
+						jQuery('#aesop-audio-<?php echo $hash;?>').waypoint({
+							offset: 'bottom-in-view',
+							handler: function(direction){
+						   		jQuery('#aesop-audio-<?php echo $hash;?> .mejs-playpause-button button').trigger('click');
+
+						   	}
+						});
+						<?php if ('on' == $atts['viewend']) { ?>
+						jQuery('#aesop-audio-<?php echo $hash;?>').waypoint({
+							handler: function(direction){
+						   		jQuery('#aesop-audio-<?php echo $hash;?> .mejs-playpause-button button').trigger('click');
+						   	}
+						});
+						<?php } ?>
+			    	});
+			    	</script>
+	   			<?php } ?>
+
+	   			<?php
+	   			do_action('aesop_audio_inside_top'); //action
+
+	   				if ($title) {
+	   					echo $title;
+	   				}
+
+	   				echo wp_audio_shortcode(  array( 'src' => $atts['src'], 'loop' => $atts['loop'] ) );
+
+	   			do_action('aesop_audio_inside_bottom'); //action
+
+	   		?></aside><?php
+
+	   	do_action('aesop_audio_after'); //action
+
+        return ob_get_clean();
+	}
+}
---
+++ public/includes/components/component-map.php
@@ -0,0 +1,87 @@
+<?php
+
+if (!function_exists('aesop_map_shortcode')) {
+	function aesop_map_shortcode($atts, $content = null) {
+
+		$defaults = array(
+			'height' 				=> 500,
+		);
+
+		wp_enqueue_script('aesop-map-script',AI_CORE_URL.'/public/includes/libs/leaflet/leaflet.js');
+		wp_enqueue_style('aesop-map-style',AI_CORE_URL.'/public/includes/libs/leaflet/leaflet.css', AI_CORE_VERSION, true);
+
+		$atts = apply_filters('aesop_map_defaults',shortcode_atts($defaults, $atts));
+
+		$hash = rand();
+
+		// actions
+		$actiontop = do_action('aesop_map_before'); //action
+		$actionbottom = do_action('aesop_map_after'); //action
+
+		//clean height
+		$height = preg_replace('/[^0-9]/','',$atts['height']);
+
+		// custom classes
+		$classes = function_exists('aesop_component_classes') ? aesop_component_classes( 'map', '' ) : null;
+
+		$out = sprintf('%s<div id="aesop-map-component" class="aesop-component aesop-map-component %s" style="height:%spx"></div>%s',$actiontop, $classes, $height, $actionbottom);
+
+		return apply_filters('aesop_map_output',$out);
+	}
+
+}
+
+class AesopMapComponent {
+
+	function __construct(){
+		add_action('wp_footer', array($this,'aesop_map_loader'),20);
+	}
+
+	function aesop_map_loader(){
+
+		global $post;
+
+		$markers = isset( $post ) ? get_post_meta($post->ID,'aesop_map_component_locations', false) : false;
+		$start = isset( $post ) ? get_post_meta($post->ID,'aesop_map_start', true) : false;
+		$mapboxid = get_option('ase_mapbox_id','aesopinteractive.hkoag9o3');
+		$zoom = isset( $post ) ? get_post_meta($post->ID,'aesop_map_component_zoom', true) : 12;
+
+		if ( function_exists('aesop_component_exists') && aesop_component_exists('map') && is_single() )  { ?>
+			<!-- Aesop Locations -->
+			<script>
+
+				var map = L.map('aesop-map-component',{
+					scrollWheelZoom: false,
+					zoom: <?php echo wp_filter_nohtml_kses( round( $zoom ) );?>,
+					center: [<?php echo $start;?>]
+				});
+
+				L.tileLayer('//{s}.tiles.mapbox.com/v3/<?php echo $mapboxid;?>/{z}/{x}/{y}.png', {
+					maxZoom: 20
+				}).addTo(map);
+
+				<?php
+
+				if($markers):
+
+					foreach($markers as $marker):
+
+						$lat 	= sanitize_text_field($marker['lat']);
+						$long 	= sanitize_text_field($marker['long']);
+						$text 	= $marker['content'] ? $marker['content'] : false;
+
+						$loc 	= sprintf('%s,%s',$lat,$long);
+
+						?> L.marker([<?php echo $loc;?>]).addTo(map).bindPopup("<?php echo $text;?>").openPopup(); <?php
+
+					endforeach;
+
+				endif;
+				?>
+			</script>
+
+		<?php }
+	}
+
+}
+new AesopMapComponent;
