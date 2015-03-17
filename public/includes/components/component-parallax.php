baee7afa66a454dcd4a59d98175fb54af5dfea5a
nphaskins <nphaskins@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-17 19:00:51 +0000

    aesop 1.5.1
    git-svn-id: https://plugins.svn.wordpress.org/aesop-story-engine/tags/1.5.1@1114817 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ public/includes/components/component-parallax.php
@@ -0,0 +1,160 @@
+<?php
+
+/**
+ * Creates a parallax component with background image, caption, lightbox, and optional "floater" item which can also be parallax, with multiple position and directions.
+ *
+ * @since    1.0.0
+ */
+
+if ( ! function_exists( 'aesop_parallax_shortcode' ) ) {
+
+	function aesop_parallax_shortcode( $atts ) {
+
+		$defaults = array(
+			'img'     			=> '',
+			'height'    		=> 500, // deprecated 1.4.2
+			'parallaxbg'   		=> 'on',
+			'floater'    		=> '',
+			'floatermedia'   	=> '',
+			'floaterposition'  	=> 'right',
+			'floaterdirection' 	=> 'up',
+			'caption'  			=> '',
+			'captionposition' 	=> 'bottom-left',
+			'lightbox'    		=> false
+		);
+
+		$atts = apply_filters( 'aesop_parallax_defaults', shortcode_atts( $defaults, $atts ) );
+
+		// let this be used multiple times
+		static $instance = 0;
+		$instance++;
+		$unique = sprintf( '%s-%s', get_the_ID(), $instance );
+
+		// add a css class if parallax bg is set to on
+		$laxclass  = 'on' == $atts['parallaxbg'] ? 'is-parallax' : false;
+
+		// add custom css classes through our utility function
+		$classes = aesop_component_classes( 'parallax', '' );
+
+		// automatically provide an alt tag for the image based on the name of the image file
+		$auto_alt  = $atts['img'] ? basename( $atts['img'] ) : null;
+
+		ob_start();
+
+		do_action( 'aesop_parallax_before' ); // action
+
+		?><div id="aesop-parallax-component-<?php echo esc_attr( $unique );?>" <?php echo aesop_component_data_atts( 'parallax', $unique, $atts );?> class="aesop-component aesop-parallax-component <?php echo sanitize_html_class( $classes );?>"><?php
+
+		do_action( 'aesop_parallax_inside_top' ); // action
+
+		// only run parallax if not on mobile and parallax is on
+		if ( ! wp_is_mobile() && ( 'on' == $atts['parallaxbg'] || 'on' == $atts['floater'] ) ) { ?>
+			<script>
+				jQuery(document).ready(function($){
+
+					<?php if ( 'on' == $atts['parallaxbg'] ) { ?>
+
+						var img 	  = $('.aesop-parallax-sc.aesop-parallax-sc-<?php echo esc_attr( $unique );?> .aesop-parallax-sc-img')
+						, 	setHeight = function() {
+
+								img.parent().imagesLoaded( function() {
+
+									var imgHeight 		= img.height()
+									,	imgCont     	= img.parent()
+
+									imgCont.css('height',Math.round(imgHeight * 0.69))
+
+									if ( $(window).height < 760 ) {
+										imgCont.css('height',Math.round(imgHeight * 0.65))
+									}
+								});
+
+							}
+
+						setHeight();
+
+						$(window).resize(function(){
+							setHeight();
+						})
+
+			   			img.parallax({speed: 0.1});
+
+	        			<?php }//end if
+
+						if ( 'on' == $atts['floater'] ) { ?>
+
+							var obj = $('.aesop-parallax-sc.aesop-parallax-sc-<?php echo esc_attr( $unique );?> .aesop-parallax-sc-floater');
+
+					       	function scrollParallax(){
+					       	    var height 			= obj.height(),
+		        					offset 			= obj.offset().top,
+					       	    	scrollTop 		= $(window).scrollTop(),
+					       	    	windowHeight 	= $(window).height(),
+					       	    	floater 		= Math.round( (offset - scrollTop) * 0.1);
+
+					       	    // only run parallax if in view
+					       	    if (offset >= scrollTop + windowHeight) {
+									return;
+								}
+
+					       	    <?php if ( 'left' == $atts['floaterdirection'] || 'right' == $atts['floaterdirection'] ) {
+
+							if ( 'left' == $atts['floaterdirection'] ) { ?>
+						            		obj.css({'transform':'translate3d(' + floater + 'px, 0px, 0px)'});
+						            	<?php } else { ?>
+											obj.css({'transform':'translate3d(-' + floater + 'px, 0px, 0px)'});
+						            	<?php }
+
+							} else {
+
+							if ( 'up' == $atts['floaterdirection'] ) { ?>
+						            		obj.css({'transform':'translate3d(0px,' + floater + 'px, 0px)'});
+										<?php } else { ?>
+											obj.css({'transform':'translate3d(0px,-' + floater + 'px, 0px)'});
+										<?php }
+							} ?>
+					   	}
+
+				    	scrollParallax();
+				    	$(window).scroll(function() {scrollParallax();});
+
+				    <?php }//end if ?>
+				});
+			</script>
+		<?php }//end if ?>
+
+			<figure class="aesop-parallax-sc aesop-parallax-sc-<?php echo esc_attr( $unique );?>">
+
+				<?php do_action( 'aesop_parallax_inner_inside_top' ); // action ?>
+
+				<?php if ( 'on' == $atts['floater'] ) {?>
+					<div class="aesop-parallax-sc-floater floater-<?php echo sanitize_html_class( $atts['floaterposition'] );?>" data-speed="10">
+						<?php echo aesop_component_media_filter( $atts['floatermedia'] );?>
+					</div>
+				<?php } ?>
+
+				<?php if ( 'on' == $atts['lightbox'] ) {?>
+					<a class="aesop-lb-link aesop-lightbox" rel="lightbox" title="<?php echo esc_attr( $atts['caption'] );?>" href="<?php echo esc_url( $atts['img'] );?>"><i class="aesopicon aesopicon-search-plus"></i></a>
+				<?php } ?>
+
+				<img class="aesop-parallax-sc-img <?php echo $laxclass;?>" src="<?php echo esc_url( $atts['img'] );?>" alt="<?php echo esc_attr( $auto_alt );?>" >
+
+				<?php if ( $atts['caption'] ) { ?>
+					<figcaption class="aesop-parallax-sc-caption-wrap <?php echo sanitize_html_class( $atts['captionposition'] );?>">
+						<?php echo aesop_component_media_filter( trim( $atts['caption'] ) );?>
+					</figcaption>
+				<?php } ?>
+
+				<?php do_action( 'aesop_parallax_inner_inside_bottom' ); // action ?>
+
+			</figure>
+
+			<?php do_action( 'aesop_parallax_inside_bottom' ); // action ?>
+
+		</div>
+
+		<?php do_action( 'aesop_parallax_after' ); // action
+
+		return ob_get_clean();
+	}
+}//end if
---
+++ readme.md
@@ -0,0 +1,78 @@
+# Description
+Aesop Story Engine is an open-sourced suite of tools that empower developers to build feature-rich, interactive, long-form stories within Wordpress. ASE is the same engine that drives the storytelling experience through our hosted platform at [Story.AM](https://story.am).
+
+---
+
+### Installation
+##### Uploading in WordPress Dashboard
+
+1. Navigate to 'Add New' in the plugins dashboard  
+2. Navigate to the 'Upload' area  
+3. Select `aesop-core.zip` from your computer  
+4. Click 'Install Now'  
+5. Activate the plugin in the Plugin dashboard  
+
+##### Using FTP  
+
+1. Download `aesop-core.zip`  
+2. Extract the `aesop-core` directory to your computer  
+3. Upload the `aesop-core` directory to the `/wp-content/plugins/` directory  
+4. Activate the plugin in the Plugin dashboard
+
+---
+
+##### Documentation
+[http://aesopstoryengine.com/developers/](http://aesopstoryengine.com/developers/)  
+
+---
+
+### Story Components
+At the heart of ASE are the storytelling components. They include:  
+* Audio  
+* Video  
+* Content  
+* Character  
+* Gallery
+* Chapter Heading  
+* Image  
+* Location  
+* Parallax  
+* Quote  
+* Timeline  
+* Collection  
+* Document Viewer  
+
+Interactive elements are created while crafting stories, with the Story Component Creator.
+![Image](https://dl.dropboxusercontent.com/u/5594632/ase-screenshot.png)
+
+---
+
+### Changelog
+See [https://github.com/bearded-avenger/aesop-core/releases](https://github.com/bearded-avenger/aesop-core/releases)
+
+---
+
+### Translations
+Aesop Story Engine is currently available in 17 languages. We work closely with the folks over at WP Translations, and it's because of them that these translations are available.
+* Deutsch (German)
+* čeština‎ (Czech)
+* Ελληνικά (Greek)
+* Español (Spanish (Spain))
+* Français (French (France))
+* עִבְרִית (Hebrew)
+* Italiano (Italian)
+* 日本語 (Japanese)
+* Nederlands (Dutch)
+* Polski (Polish)
+* Português do Brasil (Portuguese (Brazil))
+* Română (Romanian)
+* Русский (Russian)
+* Slovenčina (Slovak)
+* Српски језик (Serbian)
+* Türkçe (Turkish)
+* 中文 (Chinese (China)
+
+---
+
+### Demo Theme  
+Here is a basic sample WordPress theme showing implementation of the story engine component design: [Aesop Story Theme](https://github.com/AesopInteractive/aesop-story-theme).
